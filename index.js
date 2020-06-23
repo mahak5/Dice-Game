54b257530c1d40654808757bc48de5e9feaadc60
mahek5 <32652066+mahek5@users.noreply.github.com>
2020-06-23 15:01:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+var randomNum1 = Math.floor(Math.random() * 6) + 1;
+var randomNum2 = Math.floor(Math.random() * 6) + 1;
+var dicenum1 = 'images/dice' + randomNum1 + '.png';
+var dicenum2 = 'images/dice' + randomNum2 + '.png';
+document.querySelectorAll('img')[0].setAttribute('src', dicenum1);
+document.querySelectorAll('img')[1].setAttribute('src', dicenum2);
+if (randomNum1 > randomNum2) document.querySelector('h1').textContent = 'Player 1 wins';
+else if (randomNum2 > randomNum1) document.querySelector('h1').textContent = 'Player 2 wins';
+else document.querySelector('h1').textContent = 'Its a tie!';
+
+function myFunc() {
+	document.querySelector('h1').textContent = 'Who is the winner?';
+	document.querySelectorAll('img')[0].setAttribute('src', 'images/dice1.png');
+	document.querySelectorAll('img')[1].setAttribute('src', 'images/dice1.png');
+}
