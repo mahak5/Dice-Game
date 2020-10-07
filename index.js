7d099a1b4c619a644a31219b8e54ee2d5fe9a463
gopikanair99 <gopikavenugopal999@gmail.com>
2020-10-07 10:53:33 +0530

    Modified JS

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ var dicenum1 = 'dice' + randomNum1 + '.png';
 var dicenum2 = 'dice' + randomNum2 + '.png';
 document.querySelectorAll('img')[0].setAttribute('src', dicenum1);
 document.querySelectorAll('img')[1].setAttribute('src', dicenum2);
-if (randomNum1 > randomNum2) document.querySelector('h1').textContent = 'Player 1 wins';
-else if (randomNum2 > randomNum1) document.querySelector('h1').textContent = 'Player 2 wins';
-else document.querySelector('h1').textContent = 'Its a tie!';
+if (randomNum1 > randomNum2) document.querySelector('h1').textContent = 'Player 1 wins!';
+else if (randomNum2 > randomNum1) document.querySelector('h1').textContent = 'Player 2 wins!';
+else document.querySelector('h1').textContent = 'Oh no !Its a tie!';
 
 function myFunc() {
-	document.querySelector('h1').textContent = 'Who is the winner?';
-	document.querySelectorAll('img')[0].setAttribute('src', 'dice1.png');
-	document.querySelectorAll('img')[1].setAttribute('src', 'dice1.png');
+    document.querySelector('h1').textContent = 'Who is the winner?';
+    document.querySelectorAll('img')[0].setAttribute('src', 'dice1.png');
+    document.querySelectorAll('img')[1].setAttribute('src', 'dice1.png');
 }
